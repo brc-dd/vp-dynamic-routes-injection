b1dfd99baa44c1e795c79ffb02ce35d4c4181a36
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2025-06-22 15:50:30 +0530

    feat: init

---
+++ .vitepress/config.ts
@@ -0,0 +1,44 @@
+import path from 'node:path'
+import { type PluginOption } from 'vite'
+import { defineConfig, type SiteConfig } from 'vitepress'
+
+export default defineConfig({
+  vite: {
+    plugins: [injectDynamicRoutes(['bar', 'baz'])]
+  }
+})
+
+function injectDynamicRoutes(slugs: string[]): PluginOption {
+  const route = path.resolve(__dirname, './internal/[slug].md')
+
+  return {
+    name: 'custom:inject-dynamic-routes',
+    configResolved(config) {
+      const siteConfig = config.vitepress!
+      siteConfig.pages.push(...slugs.map((slug) => `foo/${slug}.md`))
+      siteConfig.dynamicRoutes.push(
+        ...slugs.map((slug) => ({
+          path: `foo/${slug}.md`,
+          fullPath: `${siteConfig.srcDir}/foo/${slug}.md`,
+          route,
+          loaderPath: '/dev/null',
+          params: { slug },
+          content: `This is the content for ${slug}`
+        }))
+      )
+      siteConfig.__dirty = true
+    }
+  }
+}
+
+declare module 'vite' {
+  interface UserConfig {
+    vitepress?: SiteConfig
+  }
+}
+
+declare module 'vitepress' {
+  interface SiteConfig {
+    __dirty?: boolean
+  }
+}
---
+++ index.md
@@ -0,0 +1,4 @@
+# home
+
+- [foo/bar](foo/bar.md)
+- [foo/baz](foo/baz.md)
